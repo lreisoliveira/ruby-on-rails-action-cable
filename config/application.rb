4bc09064a8b3b2c2a23045169380ffb7e067d3b4
Leandro dos Reis Oliveira <lroliveira@fcl.com.br>
2016-08-16 10:35:32 -0300

    Alterado .gitigore e config/application.rb

--- config/application.rb
+++ config/application.rb
@@ -7,15 +7,14 @@ require 'rails/all'
 Bundler.require(*Rails.groups)
 
 module ActioncableExamples
+
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-
-    config.action_cable.disable_request_forgery_protection = true
-
     # config.action_cable.allowed_request_origins = "['http://angular.websocket/']"
 
+    config.action_cable.disable_request_forgery_protection = true
     config.middleware.insert_before 0, "Rack::Cors" do
       allow do
         origins '*'
@@ -23,7 +22,6 @@ module ActioncableExamples
       end
     end
     config.middleware.delete Rack::Lock
-
-
   end
+
 end
